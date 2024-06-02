eb05b24817232f12e8305926ab5f6e9d4e1bf96a
FatimaEzzahraAAA <150025945+FatimaEzzahraAAA@users.noreply.github.com>
2024-06-02 16:24:56 +0100

    Add files via upload

---
+++ NLP Project model/Log_reg_app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pickle 
+from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+
+model=pickle.load(open('trained_model_regression_logistique', 'rb'))
+scaler=pickle.load(open('Tfidf_vectorizer', 'rb'))
+review=st.text_input('Enter your review, our Logistic regression model will predict')
+
+if st.button('Predict'):
+    #scaler= TfidfVectorizer(max_features=2500)
+    review_scale= scaler.transform([review]).toarray()
+    result=model.predict(review_scale)
+    
+    if result[0]==0:
+        st.write('Negative review')
+    else:
+        st.write('positive review')
